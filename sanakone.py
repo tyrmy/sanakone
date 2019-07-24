7d428c378bbd789226dea38235ac24cc0bafc326
Lassi Lehtinen <lokleh@utu.fi>
2019-07-25 01:00:08 +0300

    ask_randoms() added and simplified code

--- sanakone.py
+++ sanakone.py
@@ -14,6 +14,8 @@ Bash-ohjelma "order" lajittelee tietokannan aakkosjärjestykseen.
 Tämä ei ole välttämätöntä ohjelman toimimisen kannalta.
 '''
 
+import string
+from prettytable import PrettyTable
 import os
 import unicodecsv as csv
 import time as t
@@ -21,26 +23,22 @@ import random as r
 from googletrans import Translator
 
 os.chdir('/home/lassi/Python/sanakone1.1/')
-dictionary = set()
+dictionary = {}
 
 # Ilmoittaa csv-tiedoston sanojen määrän
 def dict_size():
-	word_count = 0
-	with open('eng_fi.db') as csv_file:
-		csv_reader = csv.reader(csv_file, delimiter=',')
-		for row in csv_reader:
-			word_count = word_count+1
-	return word_count
+	global dictionary
+	return len(dictionary)
 
 # Luo dictionary kohteen csv-tiedostosta.
-def generate_dict(): # luo dictionary .csv tiedoston pohjalta
-	dictionary = {}
+def dict_from_csv(): # luo dictionary .csv tiedoston pohjalta
+	global dictionary
 	with open('eng_fi.db') as csv_file:
 		csv_reader = csv.reader(csv_file, delimiter=',')
 		next(csv_reader)
 		for row in csv_reader:
 			dictionary[row[0]] = row[1]
-	return dictionary
+	print("Nykyinen tietokannan koko = " + str(dict_size()) + " sanaa.")
 
 '''	Kysyy satunnaisen sanan dictionaryn sisältä
 	Voi ottaa vastaan joko:
@@ -48,30 +46,73 @@ def generate_dict(): # luo dictionary .csv tiedoston pohjalta
 		-"x" merkin, jolloin tietokantaan voi lisätä uuden sanan
 		-"exit", jolloin ohjelma suljetaan
 '''
+def ask_randoms(amount): 
+	global dictionary
+	print(u'Kysytään %i sanaa...' % amount)
+	picks = r.sample(list(dictionary.items()), amount)
+	vastaukset = []
+	index = 0
+	montakoOikein = 0
+	for i in picks:
+		index += 1
+		print(u'%i. %s' % (index, i[0]))
+		vastaus = raw_input("Vastaus: ").decode('utf8')
+		if vastaus == i[1].lower():
+			vastaukset.append('Oikein! (%s : %s)' % (i[0],i[1]))
+			montakoOikein += 1
+		else:
+			vastaukset.append(u'Väärin! (%s : %s)' % (i[0],i[1]))
+	print('\n')
+	print('========Tulokset========')
+	for i in vastaukset:
+		print i
+	print('========================')
+	print(u'Yhteensä %i/%i oikein!' % (montakoOikein, len(vastaukset)))
+	print('========================')
+
 def ask_random(): 
-	#generate_csv()
-	di = generate_dict()
-	pick = r.choice(list(di.items()))
+	global dictionary
+	pick = r.choice(list(dictionary.items()))
 	print(u'Käännä "' + pick[0] + '"')
 	vastaus = raw_input("Vastaus: ").decode('utf8')
 	if vastaus == "x":
 		fix = raw_input("Anna uusi sana: ")
 		add_word(fix)
-		print("Tietokannan koko = " + str(dict_size()) + " sanaa.")
+		dict_from_csv()
 	elif vastaus == "exit":
 		exit()
 	else:
 		print('Ref: "' + pick[1] + '"')
 
-def add_word(word):
-	if word not in 
-	with open("eng_fi.db", "a") as fd:
-		tr = Translator()
-		result = tr.translate(word, dest='fi')
-		print(word + "," + result.text)
-		output = word + "," + result.text
-		fd.write(output.encode('utf-8') + "\n")
+def print_by_letter(key):
+	print('Taulukko %s-kirjaimen mukaan:' % key)
+	global dictionary
+	word_count = 0
+	t = PrettyTable()
+	t.field_names = ['Eng','Fi']
+	for eng, fi in sorted(dictionary.items()):
+		if eng[0] == key:
+			t.add_row([eng, fi])
+			word_count += 1
+	print(t)
+	print(u'Yhteensä %s sanaa' % word_count)
 
-print("Nykyinen tietokannan koko = " + str(dict_size()) + " sanaa.")
+def add_word(word):
+	global dictionary
+	if word not in dictionary:
+		with open("eng_fi.db", "a") as fd:
+			tr = Translator()
+			result = tr.translate(word, dest='fi')
+			print(word + "," + result.text)
+			output = word + "," + result.text
+			fd.write(output.encode('utf-8') + "\n")
+	else:
+		print('Sana ei kelpaa tai se on jo tietokannassa...')
+
+dict_from_csv()
+#for letter in list(string.ascii_lowercase):
+#	print_by_letter(letter)
 while True:
 	ask_random()
+
+#ask_randoms(20)
