67b4e7075fe71c7dc56208541fd8c91a5ec01bc8
Lassi Lehtinen <lokleh@utu.fi>
2019-08-11 18:14:57 +0300

    vocabulary is automatically generated and sorted by alphabets

--- generate_vocabulary.py
+++ generate_vocabulary.py
@@ -9,6 +9,7 @@ from datetime import datetime
 import unicodecsv as csv
 import time as t
 import random as r
+import re
 
 os.chdir('/home/lassi/Python/sanakone1.1/')
 filename = './vocabularies/vocabulary_%s.txt' % str(datetime.now()).split('.')[0]
@@ -35,14 +36,23 @@ def generate_dict():
 
 def generate_vocabulary():
 	generate_dict()
+	global dictionary
 	alphabet = []
 	for letter in range(65, 91):
     		alphabet.append(chr(letter))
-	global dictionary
 
 	with open(filename, 'w') as output:
 		#for eng, fi in dictionary.items():
 		for character in alphabet:
 			output.write('[%s] \n' % character)
+			words = []
+			for eng, fi in dictionary.items():
+				if re.match('^%s.*' % character.lower(), eng) != None:
+					word = '%s - %s \n' % (eng, fi)
+					words.append(word)
+			words.sort()
+			for word in words:
+					output.write(word.encode('utf-8'))
+			output.write('\n')	
 
 generate_vocabulary()
